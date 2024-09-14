0eabb9283cec79f4c0c2c199d9a10eaf0560bc5a
202213768 <jonathan.202213768@unilasalle.edu.br>
2024-09-13 21:04:21 -0300

    first try

---
+++ app/src/main/java/com/example/receitaslasalle/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.receitaslasalle
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var username: EditText
+    lateinit var pass: EditText
+    lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        username = findViewById(R.id.username)
+        pass = findViewById(R.id.password)
+        loginButton = findViewById(R.id.login)
+
+        loginButton.setOnClickListener {
+            if ((username.text.toString().equals("Jonathan")) && (pass.text.toString().equals("1234"))) {
+                Toast.makeText(this, "Deu certo!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
