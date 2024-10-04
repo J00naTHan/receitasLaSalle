02dbb6d2aa273d3b8ff8568d03710a5f030460fc
Guilherme <fifteenorc@gmail.com>
2024-10-04 00:59:24 -0300

    o que deu pra fazer

--- app/src/main/java/com/example/receitaslasalle/MyAdapter.kt
+++ app/src/main/java/com/example/receitaslasalle/MyAdapter.kt
@@ -1,26 +1,27 @@
 package com.example.receitaslasalle
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
+class MyAdapter(private val dataList: List<String>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
-class MyAdapter(private val dataList:List<String>): RecyclerView.Adapter:<MyAdapter.MyViewHolder>(){
-    class MyViewHolder(itemView: View ) : RecyclerView.ViewHolder(itemView) {
-        val textViewItem: TextView = itemView.findViewByID(R.id.textViewItem)
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textViewItem: TextView = itemView.findViewById(R.id.textViewItem)
     }
-    override fun on CreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayouInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
-        return MyViewHolder (view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recipe, parent, false)
+        return MyViewHolder(view)
     }
+
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.textViewItem.text = dataList[position]
     }
-    override fun getItemCount(): Int {
-        return dataList.size
-    }
 
+    override fun getItemCount(): Int {
+        return this.dataList.size
+    }
 }
