3cfd2972f118b025963e7c585ec75ac67a437f49
Guilherme <fifteenorc@gmail.com>
2024-10-04 00:51:39 -0300

    o que deu pra fazer

--- app/src/main/java/com/example/receitaslasalle/Tela2.kt
+++ app/src/main/java/com/example/receitaslasalle/Tela2.kt
@@ -5,10 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerViewAccessibilityDelegate
+
 
 class Tela2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela2)
+
+        val dataList = listOf( "item 1", "item 2", "item 3", "item 4", "item 5")
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MyAdapter(dataList)
     }
 }
